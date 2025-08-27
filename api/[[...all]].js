8ed59998aea1d04fffeb3ff535dd462f131af452
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2025-08-27 04:46:05 -0500

    deploy

--- api/[[...all]].js
+++ api/[[...all]].js
@@ -1,6 +1,3 @@
 // api/[[...all]].js
 import app from "../src/index.js";
-
-export default function handler(req, res) {
-  return app(req, res);
-}
+export default function handler(req, res) { return app(req, res); }
