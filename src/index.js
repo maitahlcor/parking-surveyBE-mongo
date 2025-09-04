6790bbaa993a90dc89eec57534338ff54ae6892a
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2025-09-04 06:30:56 -0500

    puapi auth

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.get("/health", (_req, res) => res.json({ ok: true }));
 
 /* ====== Rutas ====== */
 // OJO: tu backend ya expone /api para estos recursos
-app.use("/auth", authLocalRouter);
+app.use("/api/auth", authLocalRouter);
 app.use("/api/encuestas", encuestasRouter);
 app.use("/api/respuestas", respuestasRouter);
 
